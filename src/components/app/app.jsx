bbefa1710460c9d33092f11a0e518c4f9f55ab6f
MEDOYED <mmax26072005@gmail.com>
2024-12-28 18:43:49 +0100

    create Header as component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,25 +1,16 @@
 import { Link, Outlet } from "react-router-dom";
 
+import Header from "../widgets/header/header";
+
 import "./app.css";
 
 function App() {
   return (
     <>
       <div className="app-container">
-        <Link className="btn-test" to={"/"}>
-          Home
-        </Link>
-        <Link className="btn-test" to={"/profile"}>
-          Profile
-        </Link>
-        <Link className="btn-test" to={"/registration"}>
-          Registration
-        </Link>
-        <Link className="btn-test" to={"/signup"}>
-          Sign Up
-        </Link>
+        <Header />
+        <Outlet />
       </div>
-      <Outlet />
     </>
   );
 }
