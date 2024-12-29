4d60cd05c3b53fb4a59ff96e038e3d354ca62914
MEDOYED <mmax26072005@gmail.com>
2024-12-30 00:02:07 +0100

    layout raport-page

---
+++ src/components/pages/raport-page/report-page.jsx
@@ -0,0 +1,29 @@
+import "./report-page.scss";
+
+const RaportPage = () => {
+  return (
+    <>
+      <section className="raport-page raport">
+        <h1>Отчет</h1>
+        <ul className="raport__options">
+          <li className="raport__option active">Тональность</li>
+          <li className="raport__option">Эмоции</li>
+          <li className="raport__option">Голоса</li>
+        </ul>
+        <p className="raport__description">
+          Тут будет описываться сам отчет по каждой категории отдельно Тут будет описываться сам
+          отчет по каждой категории отдельно Тут будет описываться сам отчет по каждой категории
+          отдельно Тут будет описываться сам отчет по каждой категории отдельно Тут будет
+          описываться сам отчет по каждой категории отдельно Тут будет описываться сам отчет по
+          каждой категории отдельно
+        </p>
+
+        <a class="raport__download-btn" href="path/to/your/file.pdf" download>
+          Скачать отчет
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default RaportPage;
