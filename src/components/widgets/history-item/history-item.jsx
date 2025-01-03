111eb89f7e9520ce932d6d6fc8f97719b2403825
MEDOYED <mmax26072005@gmail.com>
2025-01-03 01:03:21 +0100

    history-item | colorize the process

--- src/components/widgets/history-item/history-item.jsx
+++ src/components/widgets/history-item/history-item.jsx
@@ -4,13 +4,23 @@ import playIcon from "../../../assets/img/play-icon.svg";
 import "./history-item.scss";
 
 const HistoryItem = ({ date, time, process }) => {
+  let processClassName;
+
+  if (process === "ошибка") {
+    processClassName = "process error";
+  } else if (process === "завершено") {
+    processClassName = "process completed";
+  } else {
+    processClassName = "process";
+  }
+
   return (
     <div>
       <li className="profile-page__history-item">
         <h2 className="title">Название</h2>
         <div className="date">{date}</div>
         <time className="time">{time}</time>
-        <div className="process">{process}</div>
+        <div className={processClassName}>{process}</div>
         <button>
           <img className="download-icon" src={downloadIcon} alt="download icon" />
         </button>
