5ee1f2903c912fa957897fa4cf430fe9d1adfa03
MEDOYED <mmax26072005@gmail.com>
2025-01-04 22:57:49 +0100

    improved @media 320px-480px on the profile page

--- src/components/widgets/history-section/history-section.jsx
+++ src/components/widgets/history-section/history-section.jsx
@@ -8,6 +8,7 @@ const HistorySection = () => {
   return (
     <div className="profile-page__history-wrapper">
       <div className="profile-page__title history">История скачиваний</div>
+      <img className="filter-img" src={filterIcon} alt="filter icon" />
       <div className="profile-page__history">
         <ul className="profile-page__history-list">
           <HistoryItem date={"22.12.24"} time={"14:17"} process={"ошибка"} />
@@ -15,9 +16,6 @@ const HistorySection = () => {
           <HistoryItem date={"21.12.24"} time={"18:21"} process={"завершено"} />
           <HistoryItem date={"21.12.24"} time={"18:21"} process={"в процессе"} />
         </ul>
-        <button>
-          <img className="filter-img" src={filterIcon} alt="filter icon" />
-        </button>
       </div>
     </div>
   );
